73049fbef43bd7441cebac0b8f36dcac62c1c5ae
Divyesh Shrestha <rashudshrestha12@gmail.com>
2022-11-17 13:48:53 +1100

    My first commit

---
+++ app.js
@@ -0,0 +1,95 @@
+const menuItems = [
+{
+    id:1,
+    item:"Food item1",
+    oneliner:"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nemo, accusantium illum  sint incidunt, voluptate esse quaerat saepe dolor." ,
+    img:"images/food.jpg",
+    hotel:"Hotel1 name1",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate aspernatu.",
+},
+{
+    id:2,
+    item:"Food item2",
+    oneliner:"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nemo, accusantium illum  sint incidunt, voluptate esse quaerat saepe dolor." ,
+    img:"images/food1.jpg",
+    hotel:"Hotel2 name2",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate aspernatu.",
+},
+{
+    id:3,
+    item:"Food item3",
+    oneliner:"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nemo, accusantium illum  sint incidunt, voluptate esse quaerat saepe dolor." ,
+    img:"images/food2.jpg",
+    hotel:"Hotel3 name3",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate aspernatu.",
+},
+{
+    id:4,
+    item:"Food item4",
+    oneliner:"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nemo, accusantium illum  sint incidunt, voluptate esse quaerat saepe dolor." ,
+    img:"images/food3.jpg",
+    hotel:"Hotel4 name4",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate aspernatu.",
+},
+{
+    id:5,
+    item:"Food item5",
+    oneliner:"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nemo, accusantium illum  sint incidunt, voluptate esse quaerat saepe dolor." ,
+    img:"images/food.jpg",
+    hotel:"Hotel5 name5",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate aspernatu.",
+}
+]
+
+   
+const item= document.getElementById("item")
+const oneliner= document.getElementById("oneliner")
+const img= document.getElementById("img")
+const hotel= document.getElementById("hotel")
+const desc= document.getElementById("desc")
+const prev= document.getElementById("prev")
+const random= document.getElementById("random")
+const next= document.getElementById("next")
+
+let currentMenu=0;
+
+window.addEventListener("DOMContentLoaded", function(){
+    // let menu=menuItems[currentMenu]
+    // item.textContent=menu.item;
+    // oneliner.textContent=menu.oneliner;
+    // img.src=menu.img;
+    // hotel.textContent=menu.hotel;
+    // desc.textContent=menu.desc;
+    allMenu(currentMenu)
+});
+const allMenu=()=>{
+    let menu=menuItems[currentMenu]
+    item.textContent=menu.item;
+    oneliner.textContent=menu.oneliner;
+    img.src=menu.img;
+    hotel.textContent=menu.hotel;
+    desc.textContent=menu.desc;
+}
+
+//random menu
+random.addEventListener("click",()=>{
+    currentMenu=Math.floor(Math.random()*menuItems.length)
+    // console.log(currentMenu)
+    allMenu(currentMenu)
+})
+//previous button
+prev.addEventListener("click",()=>{
+    currentMenu--;
+    if(currentMenu<0){
+        currentMenu=menuItems.length-1
+    }
+    allMenu(currentMenu)
+})
+//next button
+next.addEventListener("click",()=>{
+    currentMenu++;
+    if(currentMenu>menuItems.length-1){
+        currentMenu=0
+    }
+    allMenu(currentMenu)
+})
